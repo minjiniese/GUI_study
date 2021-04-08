ae528ef12af5b87d795cc90a69efaa43dc459f1d
minjin <kmj101012@naver.com>
2021-04-08 09:47:40 +0900

    5_listbox

---
+++ gui_basic/5_listbox.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+root = Tk()
+root.title("Minjin GUI")
+root.geometry("640x480") #가로 * 세로
+
+listbox = Listbox(root, selectmode="extended", height=0) #entended는 여러개 선택 가능, single은 하나만 선택 가능 , height는 0으로 하면 전체 다보임 특정 값을 주면 그 값만큼만 보임
+listbox.insert(0,"사과")
+listbox.insert(1,"딸기")
+listbox.insert(2,"바나나")
+listbox.insert(END,"수박") #끝에 추가
+listbox.insert(END,"포도")
+listbox.pack()
+
+def btncmd():
+    #삭제
+    #listbox.delete(END) #맨 뒤에 항목을 삭제, 0이면 맨앞 항목을 삭제
+    
+    #갯수 확인
+    #print("리스트에는", listbox.size(), "개가 있어요")
+
+    #항목 확인 (시작 idx, 끝 idx)
+    #print("1번째 부터 3번째까지의 항목 : ",listbox.get(0,2))
+
+    #선택된 항목 확인 (위치로 반환 (ex) (1,2,3))
+    print("선택된 항목 : ", listbox.curselection()) #current selection 이라는 뜻
+
+btn = Button(root, text="클릭", command=btncmd )
+btn.pack()
+
+root.mainloop() #창이 닫히지 않도록 함
